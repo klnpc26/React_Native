28de46842c6b69affa68d7a842f49bf05d1982a9
klnpc26 <kaiquelin12.kl@gmail.com>
2022-08-07 10:27:30 -0300

    Navegação

--- tasks/index.js
+++ tasks/index.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import Auth from './src/screens/Auth';
+import Navigator from './src/Navigator';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Auth);
+AppRegistry.registerComponent(appName, () => Navigator);
--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ImageBackground, Text, StyleSheet, View, TextInput, TouchableOpacity, Platform } from 'react-native'
+import { ImageBackground, Text, StyleSheet, View, TextInput, TouchableOpacity, Platform, Alert } from 'react-native'
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 
@@ -10,7 +10,16 @@ export default class Auth extends Component {
         email: '',
         password: '',
         confirmPassword: '',
-        stageNew: true
+        stageNew: false
+    }
+
+    signinOrSignup = () => {
+        if (this.state.stageNew) {
+            Alert.alert('Sucesso!', 'Criar conta')
+        }
+        else {
+            Alert.alert('Sucesso!', 'Logar')
+        }
     }
 
     render() {
@@ -45,13 +54,16 @@ export default class Auth extends Component {
                         />
                     }
 
-
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={this.signinOrSignup}>
                         <View style={styles.button}>
                             <Text style={styles.buttonText}>
                                 {this.state.stageNew ? 'Registrar' : 'Entrar'}
                             </Text>
                         </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{padding: 10}} onPress={() => this.setState({stageNew: !this.state.stageNew})}>
+                        <Text style={styles.buttonText}>{this.state.stageNew ? 'Já possui conta?' : 'Ainda não possui conta?'}</Text>
                     </TouchableOpacity>
                 </View>
             </ImageBackground>
